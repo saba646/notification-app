c073fc8105bca823b8485cec9bbe71ce5743bea7
saba646 <alihaaliha746@gmail.com>
2025-01-16 07:50:53 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NotificationForm from './NotificationForm';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Notification Service</h1>
+            <NotificationForm />
+        </div>
+    );
+}
+
+export default App;
